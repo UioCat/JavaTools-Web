f29677d4c50898713d805e5ee70a735fa383806c
Palm Civet <palmcivet_email@qq.com>
2021-06-29 13:04:38 +0800

    :wrench: 减少依赖项

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
 
-import "element-plus/lib/theme-chalk/index.css";
-import "element-plus/packages/theme-chalk/src/base.scss";
-
 import { router } from "./router";
 import { store } from "./stores";
 import App from "./App.vue";
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import alias from "@rollup/plugin-alias";
-import styleImport from "vite-plugin-style-import";
 
 export default defineConfig({
   plugins: [
