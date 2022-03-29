b244b9e2fa6416c9d368b886e53a18ecda8a51ac
Palm Civet <palmcivet_email@qq.com>
2022-03-30 00:12:39 +0800

    :sparkles: vite 中导入环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import alias from "@rollup/plugin-alias";
@@ -6,28 +6,29 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-export default defineConfig({
-  base: "/tools-web-test/",
-  server: {
-    port: 10000,
-    proxy: {
-      "/api": {
-        target: "https://www.uiofield.top/serve_test/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace("/api", ""),
+export default ({ mode }) =>
+  defineConfig({
+    base: `${loadEnv(mode, "").VITE_BASE_PATH}/`,
+    server: {
+      port: 10000,
+      proxy: {
+        "/api": {
+          target: "https://www.uiofield.top/serve_test/",
+          changeOrigin: true,
+          rewrite: (path) => path.replace("/api", ""),
+        },
       },
     },
-  },
-  plugins: [
-    alias({
-      entries: [{ find: "@", replacement: resolve(__dirname, "./src") }],
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    vue(),
-  ],
-});
+    plugins: [
+      alias({
+        entries: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      vue(),
+    ],
+  });
